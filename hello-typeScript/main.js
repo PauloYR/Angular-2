ef853d3faa62ba854add8e80e9186aa9796cb2f3
PauloYR <pauloyr@outlook.com>
2018-05-19 16:10:55 -0300

    TypeScript

---
+++ hello-typeScript/main.js
@@ -0,0 +1,22 @@
+var Variavel = 'teste type Script ';
+function minhaFunc(x, y) {
+    return x + y;
+}
+//ES 6 OU ES 2015
+var num = 2;
+var pi = 3.14;
+var numero = [1, 2, 3];
+numero.map(function (valor) {
+    return valor * 2;
+});
+numero.map(function (valor) { valor * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = "String";
+n1 = 1;
---
+++ hello-typeScript/main.ts
@@ -0,0 +1,23 @@
+var Variavel = 'teste type Script ';
+function minhaFunc(x, y) {
+	return x + y ;
+}
+
+//ES 6 OU ES 2015
+let num = 2;
+const pi = 3.14;
+
+var numero = [1 , 2, 3];
+numero.map(function (valor) {
+	return valor * 2 ;	
+})
+numero.map(valor => { valor * 2})
+
+class Matematica{
+	soma(x, y){
+		return x + y ;
+	}
+}
+
+let n1:any = "String";
+n1 = 1;
